9a421e923f3818c947f155c199e4801ae5cc51d2
SYED ALI FATHIMA S <146570286+FATHIMALI@users.noreply.github.com>
2026-03-22 16:29:30 +0530

    Initialize home.py with project description
    Added project overview and structure details for the Automatic Supermarket System.

---
+++ home.py
@@ -0,0 +1,24 @@
+
+The Automatic Supermarket System is a Python-based application designed to manage supermarket operations efficiently. It includes modules for Admin, Customers, and Shops to handle products, users, and transactions.
+ Features
+ User Authentication (Admin, Customer, Shop)
+ Product Management (Add, Edit, Delete, View)
+ Customer Profile Management
+ Shop Registration & Management
+ Purchase System
+ Offers & Discounts Management
+ Feedback System
+ Admin Dashboard
+
+Technologies Used
+  python
+  HTML / CSS (Frontend)
+  MySQL (Database)
+  Bootstrap (UI Design)
+Structure
+admin→ Admin functionalities
+customer → Customer operations
+shops → Shop management
+database → Database files
+images → UI images
+index.html → Homepage
